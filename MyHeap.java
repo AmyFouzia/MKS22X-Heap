3ff00b140a17277251baa12a79bb87fa4580e0a7
fuadbinzia <afouzia00@stuy.edu>
2019-05-01 00:16:36 -0400

    working on pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -3,21 +3,36 @@ import java.util.*;
 public class MyHeap{
 
   private static void pushDown(int[]data, int size, int index){
-    boolean isSorted = false; //keep track of order
-
-    int rightKid = index * 2 + 2;
-    int leftKid = index * 2 + 1;
-
-    //if no kid
-    if(){}
-    //if right kid
-    if(){}
-    //if left kid
-    if(){}
       /*- size  is the number of elements in the data array.
       - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
       - precondition: index is between 0 and size-1 inclusive
       - precondition: size is between 0 and data.length-1 inclusive.*/
+
+      boolean isSorted = false; //keep track of order
+
+      while(!isSorted){
+        int rightKid = index * 2 + 2;
+        int leftKid = index * 2 + 1;
+        int max = 0;
+
+        //if no kid
+        if(leftKid >= size && rightKid >= size){
+          isSorted = true;
+        }
+        else{
+          max = data[index];
+        }
+
+        //if right kid
+        if(rightKid < size){
+
+        }
+
+        //if left kid
+        if(leftKid < size){
+          
+        }
+      }
   }
 
   private static void pushUp(int[]data, int index){
